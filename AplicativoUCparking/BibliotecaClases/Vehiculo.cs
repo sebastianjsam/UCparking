920aa679c966de6bc6c6a577b03efadf64a21d5e
David Clavijo <dfclavijo@gmail.com>
2019-08-09 11:17:31 -0500

    Clase Vehiculo

---
+++ AplicativoUCparking/BibliotecaClases/Vehiculo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BibliotecaClases
+{
+    public class Vehiculo
+    {
+        public int VHid { get; set; }
+        public string Placa { get; set; }
+        public string Propietario { get; set; }
+        public string Phone { get; set; }
+        public string Marca { get; set; }
+        public string Color { get; set; }
+        public string Modelo { get; set; }
+        public string Motor { get; set; }
+    }
+}
